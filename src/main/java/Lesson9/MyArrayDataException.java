4b23677f4104b0d36895e981cd43acef347096b3
neqelovek <neqelovek@gmail.com>
2021-11-19 00:13:23 +0300

    Lesson9 finish

---
+++ src/main/java/Lesson9/MyArrayDataException.java
@@ -0,0 +1,8 @@
+package Lesson9;
+
+public class MyArrayDataException extends RuntimeException {
+
+    public MyArrayDataException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
---
+++ src/main/java/Lesson9/MyArraySizeException.java
@@ -0,0 +1,7 @@
+package Lesson9;
+
+public class MyArraySizeException extends RuntimeException {
+    public MyArraySizeException(String message) {
+        super(message);
+    }
+}
