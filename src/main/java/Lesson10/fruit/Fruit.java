a7b1a421e4a0c0b380b85310a45ba488cbbcc22b
neqelovek <neqelovek@gmail.com>
2021-11-22 01:19:23 +0300

    Lesson10 goToTheFinish

---
+++ src/main/java/Lesson10/fruit/Fruit.java
@@ -0,0 +1,13 @@
+package Lesson10.fruit;
+
+public abstract class Fruit {
+    private final float weight;
+
+    public Fruit(float weight) {
+        this.weight = weight;
+    }
+
+    public float getWeight() {
+        return weight;
+    }
+}
